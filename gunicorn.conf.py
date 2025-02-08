adf5d37a1f41a81df481bf16425319c8b2cd790e
dendencat <33782903+dendencat@users.noreply.github.com>
2025-02-08 17:28:46 +0900

    UPDATE: beautify html.

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+bind = '0.0.0.0:8000'
+workers = 3
+timeout = 300
+errorlog = '-'
+accesslog = '-'
+capture_output = True
+enable_stdio_inheritance = True
+reload = True
+wsgi_app = 'techblog.wsgi:application'
+chdir = '/app'
